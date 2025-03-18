8ac5039a1b363f7c2df54e98258af02450966efc
JammyGeeza <jammiedodgers6@hotmail.com>
2025-03-18 17:13:57 +0000

    UPDATE: Added 'Mobsanity' location checks for each enemy type accessible from Mainland.

--- worlds/stacklands/Locations.py
+++ worlds/stacklands/Locations.py
@@ -1,10 +1,16 @@
+from enum import Enum
 from typing import Dict, List, NamedTuple
 from BaseClasses import MultiWorld, Location, LocationProgressType, Region
+
+class CheckType(Enum):
+    Default = 0
+    Mobsanity = 1
 
 class LocationData(NamedTuple):
     name: str
     region: str
-    progress_type: LocationProgressType
+    progress_type: LocationProgressType = LocationProgressType.DEFAULT
+    check_type: CheckType = CheckType.Default
 
 class StacklandsLocation(Location):
     game = "Stacklands"
@@ -24,91 +30,117 @@ class StacklandsLocation(Location):
 location_table: List[LocationData] = [
 
     # 'Welcome' Category
-    LocationData("Open the Booster Pack"                               , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Drag the Villager on top of the Berry Bush"          , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Mine a Rock using a Villager"                        , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Sell a Card"                                         , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Buy the Humble Beginnings Pack"                      , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Harvest a Tree using a Villager"                     , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Make a Stick from Wood"                              , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Pause using the play icon in the top right corner"   , "Mainland", LocationProgressType.EXCLUDED),
-    LocationData("Grow a Berry Bush using Soil"                        , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Build a House"                                       , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Get a Second Villager"                               , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Create Offspring"                                    , "Mainland", LocationProgressType.DEFAULT),
+    LocationData("Open the Booster Pack"                               , "Mainland"),
+    LocationData("Drag the Villager on top of the Berry Bush"          , "Mainland"),
+    LocationData("Mine a Rock using a Villager"                        , "Mainland"),
+    LocationData("Sell a Card"                                         , "Mainland"),
+    LocationData("Buy the Humble Beginnings Pack"                      , "Mainland"),
+    LocationData("Harvest a Tree using a Villager"                     , "Mainland"),
+    LocationData("Make a Stick from Wood"                              , "Mainland"),
+    LocationData("Pause using the play icon in the top right corner"   , "Mainland"         , LocationProgressType.EXCLUDED),
+    LocationData("Grow a Berry Bush using Soil"                        , "Mainland"),
+    LocationData("Build a House"                                       , "Mainland"),
+    LocationData("Get a Second Villager"                               , "Mainland"),
+    LocationData("Create Offspring"                                    , "Mainland"),
     
     # 'The Grand Scheme' Category
-    LocationData("Unlock all Packs"                                    , "Mainland", LocationProgressType.EXCLUDED), # <- Achievable only from receiving items from checks
-    LocationData("Get 3 Villagers"                                     , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Find the Catacombs"                                  , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Find a mysterious artifact"                          , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Build a Temple"                                      , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Bring the Goblet to the Temple"                      , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Kill the Demon"                                      , "Mainland", LocationProgressType.DEFAULT), # <- Will be a check if 'Kill the Demon Lord' is the goal
+    LocationData("Unlock all Packs"                                    , "Mainland"         , LocationProgressType.EXCLUDED), # <- Achievable only from receiving items from checks
+    LocationData("Get 3 Villagers"                                     , "Mainland"),
+    LocationData("Find the Catacombs"                                  , "Mainland"),
+    LocationData("Find a mysterious artifact"                          , "Mainland"),
+    LocationData("Build a Temple"                                      , "Mainland"),
+    LocationData("Bring the Goblet to the Temple"                      , "Mainland"),
+    LocationData("Kill the Demon"                                      , "Mainland"), # <- Will be a check if 'Kill the Demon Lord' is the goal
     
     # 'Power & Skill' Category
-    LocationData("Train Militia"                                       , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Kill a Rat"                                          , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Kill a Skeleton"                                     , "Mainland", LocationProgressType.DEFAULT),
+    LocationData("Train Militia"                                       , "Mainland"),
+    LocationData("Kill a Rat"                                          , "Mainland"),
+    LocationData("Kill a Skeleton"                                     , "Mainland"),
     
     # 'Strengthen Up' Category
-    LocationData("Train an Archer"                                     , "Mainland", LocationProgressType.EXCLUDED), # <- Achievable mostly through RNG
-    LocationData("Make a Villager wear a Rabbit Hat"                   , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Build a Smithy"                                      , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Train a Wizard"                                      , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Equip an Archer with a Quiver"                       , "Mainland", LocationProgressType.EXCLUDED), # <- Achievable mostly through RNG
-    LocationData("Have a Villager with Combat Level 20"                , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Train a Ninja"                                       , "Mainland", LocationProgressType.DEFAULT), 
+    LocationData("Train an Archer"                                     , "Mainland"         , LocationProgressType.EXCLUDED), # <- Achievable mostly through RNG
+    LocationData("Make a Villager wear a Rabbit Hat"                   , "Mainland"),
+    LocationData("Build a Smithy"                                      , "Mainland"),
+    LocationData("Train a Wizard"                                      , "Mainland"),
+    LocationData("Equip an Archer with a Quiver"                       , "Mainland"         , LocationProgressType.EXCLUDED), # <- Achievable mostly through RNG
+    LocationData("Have a Villager with Combat Level 20"                , "Mainland"),
+    LocationData("Train a Ninja"                                       , "Mainland"), 
     
     # 'Potluck' Category
-    LocationData("Start a Campfire"                                    , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Cook Raw Meat"                                       , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Cook an Omelette"                                    , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Cook a Frittata"                                     , "Mainland", LocationProgressType.DEFAULT),
+    LocationData("Start a Campfire"                                    , "Mainland"),
+    LocationData("Cook Raw Meat"                                       , "Mainland"),
+    LocationData("Cook an Omelette"                                    , "Mainland"),
+    LocationData("Cook a Frittata"                                     , "Mainland"),
     
     # 'Discovery' Category
-    LocationData("Explore a Forest"                                    , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Explore a Mountain"                                  , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Open a Treasure Chest"                               , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Find a Graveyard"                                    , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Get a Dog"                                           , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Train an Explorer"                                   , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Buy something from a Travelling Cart"                , "Mainland", LocationProgressType.DEFAULT),
+    LocationData("Explore a Forest"                                    , "Mainland"),
+    LocationData("Explore a Mountain"                                  , "Mainland"),
+    LocationData("Open a Treasure Chest"                               , "Mainland"),
+    LocationData("Find a Graveyard"                                    , "Mainland"),
+    LocationData("Get a Dog"                                           , "Mainland"),
+    LocationData("Train an Explorer"                                   , "Mainland"),
+    LocationData("Buy something from a Travelling Cart"                , "Mainland"),
     
     # 'Ways and Means' Category
-    LocationData("Have 5 Ideas"                                        , "Mainland", LocationProgressType.EXCLUDED),
-    LocationData("Have 10 Ideas"                                       , "Mainland", LocationProgressType.EXCLUDED),
-    LocationData("Have 10 Wood"                                        , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Have 10 Stone"                                       , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Get an Iron Bar"                                     , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Have 5 Food"                                         , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Have 10 Food"                                        , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Have 20 Food"                                        , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Have 50 Food"                                        , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Have 10 Coins"                                       , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Have 30 Coins"                                       , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Have 50 Coins"                                       , "Mainland", LocationProgressType.DEFAULT),
+    LocationData("Have 5 Ideas"                                        , "Mainland"         , LocationProgressType.EXCLUDED), # <- Quite literally depends on receiving ideas from checks
+    LocationData("Have 10 Ideas"                                       , "Mainland"         , LocationProgressType.EXCLUDED), # <- Quite literally depends on receiving ideas from checks
+    LocationData("Have 10 Wood"                                        , "Mainland"),
+    LocationData("Have 10 Stone"                                       , "Mainland"),
+    LocationData("Get an Iron Bar"                                     , "Mainland"),
+    LocationData("Have 5 Food"                                         , "Mainland"),
+    LocationData("Have 10 Food"                                        , "Mainland"),
+    LocationData("Have 20 Food"                                        , "Mainland"),
+    LocationData("Have 50 Food"                                        , "Mainland"),
+    LocationData("Have 10 Coins"                                       , "Mainland"),
+    LocationData("Have 30 Coins"                                       , "Mainland"),
+    LocationData("Have 50 Coins"                                       , "Mainland"),
     
     # 'Construction' Category
-    LocationData("Have 3 Houses"                                       , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Build a Shed"                                        , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Build a Quarry"                                      , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Build a Lumber Camp"                                 , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Build a Farm"                                        , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Build a Brickyard"                                   , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Sell a Card at a Market"                             , "Mainland", LocationProgressType.DEFAULT),
+    LocationData("Have 3 Houses"                                       , "Mainland"),
+    LocationData("Build a Shed"                                        , "Mainland"),
+    LocationData("Build a Quarry"                                      , "Mainland"),
+    LocationData("Build a Lumber Camp"                                 , "Mainland"),
+    LocationData("Build a Farm"                                        , "Mainland"),
+    LocationData("Build a Brickyard"                                   , "Mainland"),
+    LocationData("Sell a Card at a Market"                             , "Mainland"),
     
     # 'Longevity' Category
-    LocationData("Reach Moon 6"                                        , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Reach Moon 12"                                       , "Mainland", LocationProgressType.DEFAULT),
-    LocationData("Reach Moon 24"                                       , "Mainland", LocationProgressType.EXCLUDED), # <- Reaching higher moons for important items is arduous
-    LocationData("Reach Moon 36"                                       , "Mainland", LocationProgressType.EXCLUDED), # <- Reaching higher moons for important items is arduous
+    LocationData("Reach Moon 6"                                        , "Mainland"),
+    LocationData("Reach Moon 12"                                       , "Mainland"),
+    LocationData("Reach Moon 24"                                       , "Mainland"         , LocationProgressType.EXCLUDED), # <- Reaching higher moons for important items is just a waiting game
+    LocationData("Reach Moon 36"                                       , "Mainland"         , LocationProgressType.EXCLUDED), # <- Reaching higher moons for important items is just a waiting game
+
+    # Mobsanity
+    LocationData("Kill a Bear"                                         , "Mainland"         , check_type=CheckType.Mobsanity),
+    # LocationData("Kill a Dark Elf"                                     , "The Dark Forest"  , check_type=CheckType.Mobsanity),
+    LocationData("Kill an Elf"                                         , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill an Elf Archer"                                  , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill an Enchanted Shroom"                            , "Mainland"         , check_type=CheckType.Mobsanity),
+    # LocationData("Kill an Ent"                                         , "The Dark Forest"  , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Feral Cat"                                    , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Frog Man"                                     , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Ghost"                                        , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Giant Rat"                                    , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Giant Snail"                                  , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Goblin"                                       , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Goblin Archer"                                , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Goblin Shaman"                                , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Merman"                                       , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Mimic"                                        , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Mosquito"                                     , "Mainland"         , check_type=CheckType.Mobsanity),
+    # LocationData("Kill an Ogre"                                        , "The Dark Forest"  , check_type=CheckType.Mobsanity),
+    # LocationData("Kill an Orc Wizard"                                  , "The Dark Forest"  , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Slime"                                        , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Small Slime"                                  , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Snake"                                        , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Tiger"                                        , "Mainland"         , check_type=CheckType.Mobsanity),
+    LocationData("Kill a Wolf"                                         , "Mainland"         , check_type=CheckType.Mobsanity),
 ]
 
 # Goal table ('Goal' option value mapped to goal name)
 goal_table: Dict[int, LocationData] = {
-    0   : LocationData("Kill the Demon",        "Mainland", LocationProgressType.DEFAULT),
-    1   : LocationData("Kill the Demon Lord",   "Mainland", LocationProgressType.DEFAULT)
+    0   : LocationData("Kill the Demon",        "Mainland"),
+    1   : LocationData("Kill the Demon Lord",   "Mainland")
 }
 
 base_id: int = 92000
