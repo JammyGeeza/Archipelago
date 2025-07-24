eb4691301719e01e9401abac0375844578d156e0
JammyGeeza <jammiedodgers6@hotmail.com>
2025-07-24 16:09:39 +0100

    Removed Deathlink from options as it has moved to in-game toggle.

--- worlds/stacklands/__init__.py
+++ worlds/stacklands/__init__.py
@@ -61,7 +61,7 @@ class StacklandsWorld(World):
     # Fill the slot data
     def fill_slot_data(self) -> Dict[str, Any]:
         slot_data = {}
-        slot_data.update(self.options.as_dict("board_expansion_mode", "board_expansion_amount", "death_link", "goal", "mobsanity", "moon_length", "packsanity", "pausing", "quest_checks", "sell_cards_trap_amount", "start_inventory"))
+        slot_data.update(self.options.as_dict("board_expansion_mode", "board_expansion_amount", "goal", "mobsanity", "moon_length", "packsanity", "pausing", "quest_checks", "sell_cards_trap_amount", "start_inventory"))
         slot_data.update({ "version": "0.1.7" })
         return slot_data
     
