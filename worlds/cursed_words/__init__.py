b431affc8f4e30b875e4f46063e7834c4765f12a
JammyGeeza <jammiedodgers6@hotmail.com>
2026-07-17 01:02:45 +0100

    Bumped version number

--- worlds/cursed_words/__init__.py
+++ worlds/cursed_words/__init__.py
@@ -13,7 +13,7 @@ class CursedWordsWeb(WebWorld):
 
     tutorials = [Tutorial(
         "Multiworld Setup Guide",
-        "A guide to setting up the Cursed Worlds randomizer connected to an Archipelago Multiworld",
+        "A guide to setting up the Cursed Words randomizer connected to an Archipelago Multiworld",
         "English",
         "setup_en.md",
         "setup\en",
